941a56eb5bb468a81784ae1a52e74ce558f55437
hseun <hseun0732@dms.hs.kr>
2025-07-16 09:47:04 +0900

    pub :: Components

---
+++ app/src/main/java/com/hackaton/pastport/ui/Components.kt
@@ -0,0 +1,38 @@
+package com.hackaton.pastport.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.hackaton.pastport.ui.theme.Main
+import com.hackaton.pastport.ui.theme.PastPortFontStyle
+
+@Composable
+fun AuthTitle(
+    modifier: Modifier = Modifier,
+    title: String
+) {
+    Column (
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = modifier.height(120.dp))
+        Text(
+            text = title,
+            style = PastPortFontStyle.bold36,
+            color = Main,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = modifier.height(60.dp))
+    }
+}
