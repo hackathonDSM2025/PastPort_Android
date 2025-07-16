2ff04c8c1b9d4336ef21ef2d51f7ebfc7d28ba79
hseun <hseun0732@dms.hs.kr>
2025-07-16 09:46:37 +0900

    add :: font

--- app/src/main/java/com/hackaton/pastport/ui/theme/Type.kt
+++ app/src/main/java/com/hackaton/pastport/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.hackaton.pastport.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hackaton.pastport.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +35,41 @@ val Typography = Typography(
     )
     */
 )
+
+val Pretendard = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.pretendard_regular,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.pretendard_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.pretendard_semibold,
+            weight = FontWeight.SemiBold
+        ),
+        Font(
+            resId = R.font.pretendard_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
+
+object PastPortFontStyle {
+    private val defaultTextColor = Black
+
+    private fun createTextStyle(weight: FontWeight, size: Int) = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = weight,
+        fontSize = size.sp,
+        color = defaultTextColor
+    )
+
+    val medium14 = createTextStyle(FontWeight.Medium, 14)
+    val medium16 = createTextStyle(FontWeight.Medium, 16)
+
+    val bold16 = createTextStyle(FontWeight.Bold, 16)
+    val bold36 = createTextStyle(FontWeight.Bold, 36)
+}
