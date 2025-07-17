d7d3c5480cf3fac3c44f1da29c411e6f2b80ceb4
hseun <hseun0732@dms.hs.kr>
2025-07-17 09:14:36 +0900

    add :: medalResource

--- app/src/main/java/com/hackaton/pastport/main/home/HomeScreen.kt
+++ app/src/main/java/com/hackaton/pastport/main/home/HomeScreen.kt
@@ -49,6 +49,7 @@ import com.hackaton.pastport.ui.theme.Main
 import com.hackaton.pastport.ui.theme.PastPortFontStyle
 import com.hackaton.pastport.ui.theme.White
 import com.hackaton.pastport.ui.utils.noRippleClickable
+import com.hackaton.pastport.utils.getMedalResource
 
 @Composable
 fun HomeScreen(
@@ -466,17 +467,5 @@ private fun LateMedal(
                 }
             }
         }
-    }
-}
-
-private fun getMedalResource(name: String): Int? {
-    return when (name) {
-        "경복궁" -> R.drawable.medal_gyeongbok
-        "불국사" -> R.drawable.medal_bulguk
-        "고인돌" -> R.drawable.medal_dolmen
-        "하회마을" -> R.drawable.medal_hahoe
-        "남한산성" -> R.drawable.medal_namhansanseong
-        "탑골공원" -> R.drawable.medal_tapgol
-        else -> null
     }
 }
