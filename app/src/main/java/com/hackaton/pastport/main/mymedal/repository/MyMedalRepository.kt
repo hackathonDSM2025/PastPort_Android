cab31b090473e22bd90281ccba07156dd91b826e
hseun <hseun0732@dms.hs.kr>
2025-07-17 09:15:09 +0900

    feature :: myMedal

---
+++ app/src/main/java/com/hackaton/pastport/main/mymedal/repository/MyMedalRepository.kt
@@ -0,0 +1,22 @@
+package com.hackaton.pastport.main.mymedal.repository
+
+import com.hackaton.pastport.network.BadgeApi
+import com.hackaton.pastport.network.UserApi
+import com.hackaton.pastport.network.model.badge.GetBadgeListResponse
+import com.hackaton.pastport.network.model.user.GetBadgeResponse
+import com.hackaton.pastport.utils.Token
+import com.hackaton.pastport.utils.apiCall
+import javax.inject.Inject
+
+class MyMedalRepository @Inject constructor(
+    private val api: BadgeApi,
+    private val token: Token
+) {
+    private val accessToken = token.getAccessToken()
+    suspend fun getMyMedal(): Result<GetBadgeListResponse> {
+        if (accessToken.isNullOrEmpty()) {
+            return Result.failure(Error("Token is not found"))
+        }
+        return apiCall("getMyMedal") { api.getBadgeList(accessToken) }
+    }
+}
